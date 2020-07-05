29f55a18ef83dc4c5313ffa2fabbdeaa4b2d408c
Nawajish Anwar Laskar <laskarnawajish@gmail.com>
2020-07-05 15:57:04 +0530

    Template finished

--- AboutSAG/www.softwareag.com/corporate/system/v1.37/assets/sag/scripts/p_search_res.js
+++
@@ -1,5 +0,0 @@
-$("#frmPartFinder").submit(function (event) {
-    // Stop form from submitting normally
-    event.preventDefault();
-    $("#results").load("partner_finder_results.html #container", $("#frmPartFinder").serialize());
-});
